9a51f6911ec4ca7966df9e0c079541c26e713dc9
David Krejci <taavetti.krejci@seznam.cz>
2020-07-08 18:34:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# show-my-resolution
+
+Super simple Vue.js app to responsively show the current resolution of your browser.
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="app">{{ this.width }} x {{ this.height }}</div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	data() {
+		return {
+			width: window.innerWidth,
+			height: window.innerHeight
+		};
+	},
+	created() {
+		window.addEventListener('resize', this.getDimensions);
+	},
+	methods: {
+		getDimensions() {
+			this.width = window.innerWidth;
+			this.height = window.innerHeight;
+		}
+	}
+};
+</script>
+
+<style>
+html,
+body {
+	margin: 0;
+	padding: 0;
+	height: 100%;
+}
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #333;
+	font-size: 15vw;
+	font-weight: bold;
+	margin: 0px;
+	height: 100%;
+	width: 100%;
+	background-color: rgb(214, 153, 40);
+}
+</style>
